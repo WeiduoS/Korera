160f9cb887d661bf2e30c5552515be4177c0b8bd
WeiduoS <ws1255@nyu.edu>
2020-01-24 16:32:37 -0500

    update

--- src/main/java/com/itlize/Korera/config/SpringSecurityConfig.java
+++ src/main/java/com/itlize/Korera/config/SpringSecurityConfig.java
@@ -7,12 +7,10 @@ import com.itlize.Korera.service.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
@@ -112,7 +110,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login", "/user/sign-up", "/user/add")
                 .permitAll()
                 .antMatchers("/**")
-                .hasAnyRole("ROLE_USER")
+                .hasAnyRole("USER")
                 .anyRequest()
                 .authenticated()
             .and().formLogin()
